48b0415b0d0f33a2a2ca8b000a867c61976f95e9
Samuel James <507.s94j@gmail.com>
2022-01-01 16:31:52 +0000

    sprite for reference

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,26 @@ fn main() {
         .run();
 }
 
-fn startup(mut commands: Commands) {
+fn startup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn().insert_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        sprite: Sprite::new(Vec2::new(32.0, 32.0)),
+        ..Default::default()
+    });
     commands
         .spawn()
+        .insert_bundle(SpriteBundle {
+            material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
+            transform: Transform::from_xyz(0.0, -215.0, 0.0),
+            sprite: Sprite::new(Vec2::new(32.0, 32.0)),
+            ..Default::default()
+        })
         .insert(Transform::default())
         .insert(Player {})
-        .insert(Speed(5.0));
+        .insert(Speed(500.0));
+
+    commands
+        .spawn()
+        .insert_bundle(OrthographicCameraBundle::new_2d());
 }
